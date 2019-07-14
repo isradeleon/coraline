6dcd83b817b9337ff615fd3f851f6ecbb9214760
isradeleon <isra.backend@gmai.com>
2019-07-14 14:30:47 -0500

    staggered view inserted elements

--- socialiteui.js
+++ socialiteui.js
@@ -8,13 +8,95 @@
         setUpStaggered();
     })
 
+    document.addEventListener('DOMNodeInserted', function(e){
+        console.log('INSERTEEEED')
+        console.log(e)
+        var parent = e.relatedNode
+        if(parent.className.includes('staggered')){
+            console.log('mmm deberia')
+            setUpStaggered();
+        }
+    })
+
+    setTimeout(() => {
+        document.querySelector('.staggered').insertAdjacentHTML(
+            'beforeend', 
+            `<div class="staggered-item">
+            <article class="post">
+    
+                <section class="post-header">
+                    <span class="user-img"
+                    style="
+                    width: 30px; height: 30px;
+                    background-image: url(https://imagenes.milenio.com/mo9ozdZ0_6gSr9na6tncVakxgsk=/958x596/https://www.milenio.com/uploads/media/2019/05/19/elizabeth-olsen-audiciono-para-ser_0_250_913_568.jpg)"></span>
+        
+                    <span class="post-data">
+                        <p>Elizabeth Olsen</p>
+                        <small>5 min ago</small>
+                    </span>
+                </section>
+    
+                <img src="http://es.web.img3.acsta.net/pictures/15/09/15/12/25/231241.jpg" alt="">
+                    
+                <section class="social-actions evenly">
+                    <button class="post-btn">
+                        <i class="far fa-heart"></i>
+                    </button>
+                    <button class="post-btn">
+                        <i class="far fa-comment"></i>
+                    </button>
+                </section>
+    
+            </article>
+        </div>`
+        );
+    }, 2000);
+
+    setTimeout(() => {
+        document.querySelector('.staggered').insertAdjacentHTML(
+            'beforeend', 
+            `<div class="staggered-item">
+            <article class="post">
+    
+                <section class="post-header">
+                    <span class="user-img"
+                    style="
+                    width: 30px; height: 30px;
+                    background-image: url(https://imagenes.milenio.com/mo9ozdZ0_6gSr9na6tncVakxgsk=/958x596/https://www.milenio.com/uploads/media/2019/05/19/elizabeth-olsen-audiciono-para-ser_0_250_913_568.jpg)"></span>
+        
+                    <span class="post-data">
+                        <p>Elizabeth Olsen</p>
+                        <small>5 min ago</small>
+                    </span>
+                </section>
+    
+                <img src="http://es.web.img3.acsta.net/pictures/15/09/15/12/25/231241.jpg" alt="">
+                    
+                <section class="social-actions evenly">
+                    <button class="post-btn">
+                        <i class="far fa-heart"></i>
+                    </button>
+                    <button class="post-btn">
+                        <i class="far fa-comment"></i>
+                    </button>
+                </section>
+    
+            </article>
+        </div>`
+        );
+    }, 4000);
+
+    var fakeMargin = 20;
     function setUpStaggered(){
         var staggered = document.querySelector('.staggered')
         var items = staggered.querySelectorAll('.staggered-item')
     
         var cols = 3;
+        fakeMargin = 20
         for (let i = 0; i < items.length; i++) {
             if(i < 3){
+                var element = items[i];
+                element.firstElementChild.style.marginBottom = fakeMargin+'px';
                 continue;
             }
             
@@ -63,10 +145,11 @@
         if(refElement.firstElementChild.staggered){
             distance+=refElement.firstElementChild.staggered;
         }
-        distance -= 20;
+        distance -= fakeMargin;
         
         element.firstElementChild.style.marginTop = '-'+distance+'px';
-        element.firstElementChild.staggered = distance
+        element.firstElementChild.staggered = distance;
+        element.firstElementChild.style.marginBottom = fakeMargin+'px';
     }
 
     function imgLoaded(imgElement) {
