5e7daa624adce1a511d00eea503f3e2a6a97db9e
isradeleon <isra.backend@gmai.com>
2019-07-17 13:14:33 -0500

    working on the docs

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 var nunjucks = require('nunjucks')
 var fs = require('fs'); 
 
-var index = nunjucks.render('website/index.njk')
+var index = nunjucks.render('website/index.njk.html')
 fs.writeFileSync('index.html', index);
+
+var docs = nunjucks.render('website/docs.njk.html')
+fs.writeFileSync('docs.html', docs);
 
 console.log('Ready!')
