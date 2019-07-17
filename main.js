968e41081c5ee0a3262343cd620edc9e7bcba8c0
isradeleon <isra.backend@gmai.com>
2019-07-17 16:30:09 -0500

    components html

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('website'));
 var index = env.render('index.njk.html')
 fs.writeFileSync('index.html', index);
 
-var docs = env.render('docs.njk.html')
-fs.writeFileSync('docs.html', docs);
+/* var docs = env.render('docs.njk.html')
+fs.writeFileSync('docs.html', docs); */
+
+var components = env.render('components.njk.html', {current: 'components'})
+fs.writeFileSync('components.html', components);
 
 console.log('Website is ready!')
