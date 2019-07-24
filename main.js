3e606f87608f29fb8909d665fa281e2e17dedb8c
isradeleon <isra.backend@gmai.com>
2019-07-23 22:17:23 -0500

    minifier added

--- main.js
+++ main.js
@@ -3,13 +3,21 @@ var fs = require('fs');
 
 var env = new nunjucks.Environment(new nunjucks.FileSystemLoader('website'));
 
+/**
+ * SOCIALITEUI.COM WEBSITE
+ */
 var index = env.render('index.njk.html')
 fs.writeFileSync('index.html', index);
 
-/* var docs = env.render('docs.njk.html')
-fs.writeFileSync('docs.html', docs); */
-
 var components = env.render('components.njk.html', {current: 'components'})
 fs.writeFileSync('components.html', components);
+
+/**
+ * CSS MINIFIER
+ */
+var csso = require('csso');
+var baseCss = fs.readFileSync('base.css')
+var minifiedCss = csso.minify(baseCss).css;
+fs.writeFileSync('socialiteui-v0.4.2/socialiteui.min.css', minifiedCss);
 
 console.log('Website is ready!')
