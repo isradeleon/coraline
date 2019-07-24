f86cefa0ec0ba1fc32962a72ab2b7dc8d9428642
isradeleon <isra.backend@gmai.com>
2019-07-23 23:10:10 -0500

    working with autoprefixer

--- main.js
+++ main.js
@@ -13,11 +13,39 @@ var components = env.render('components.njk.html', {current: 'components'})
 fs.writeFileSync('components.html', components);
 
 /**
+ * BASE CSS CODE
+ */
+var baseCss = fs.readFileSync('base.css')
+
+
+
+/**
  * CSS MINIFIER
  */
 var csso = require('csso');
-var baseCss = fs.readFileSync('base.css')
+
 var minifiedCss = csso.minify(baseCss).css;
 fs.writeFileSync('socialiteui-v0.4.2/socialiteui.min.css', minifiedCss);
+
+/**
+ * AUTOPREFIXER
+ */
+const autoprefixer = require('autoprefixer')
+const postcss = require('postcss')
+/* postcss([ autoprefixer({ overrideBrowserslist: 'last 2 versions' }) ]).process(minifiedCss).then(result => {
+    result.warnings().forEach(warn => {
+      console.warn(warn.toString())
+    })
+    console.log(result.css)
+    console.log(result)
+}) */
+
+postcss([ autoprefixer ]).process(minifiedCss).then(result => {
+    result.warnings().forEach(warn => {
+      console.warn(warn.toString())
+    })
+    console.log(result.css)
+    console.log(result)
+})
 
 console.log('Website is ready!')
